2a91ce8184d12b754bd7911bf5cbe0c396dc186b
Lucas Cristiano Passini <passini@protonmail.com>
2022-03-22 23:49:38 -0300

    add: Layout & Meals components

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
-  return <></>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="Table full of food." />
+      </div>
+      ;
+    </>
+  );
 };
 
 export default Header;
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
+
+import CartIcon from '../Cart/CartIcon.js';
+import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-  return <></>;
+  const [amountCart, setAmountCart] = useState(0);
+
+  const handleAmountCart = (event) => {
+    setAmountCart(amountCart + 1);
+  };
+
+  return (
+    <button className={classes.button} onClick={handleAmountCart}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{amountCart}</span>
+    </button>
+  );
 };
 
 export default HeaderCartButton;
