85e12d8fdf2192065ca5f1b0df8b6d4f664a340e
Lucas Cristiano Passini <passini@protonmail.com>
2022-04-10 19:51:53 -0300

    add: Cart Context & Provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
-import { useState } from 'react';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,13 +17,13 @@ function App() {
   };
 
   return (
-    <>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
